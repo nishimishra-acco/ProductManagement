3fc1a68b0d9cfa490b180305850b9b9890a8481d
nishimishra-acco <nishi@accolitedigital.com>
2024-09-16 20:15:06 +0530

    Add Product Management

--- src/ProductManagement.Data/obj/Debug/net8.0/ProductManagement.Data.AssemblyInfo.cs
+++ src/ProductManagement.Data/obj/Debug/net8.0/ProductManagement.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394ae2cd7fe78e8a0735f616d9ae04745686eb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
